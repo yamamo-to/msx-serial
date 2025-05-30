1622cd39062c8ec383e1d5eaf8ce78cd772dea6f
yamamo-to <humorum@gmail.com>
2025-05-31 00:31:33 +0900

    Patch with mock2 by cursor editor

--- tests/test_terminal_dummy.py
+++ tests/test_terminal_dummy.py
@@ -7,7 +7,9 @@ from msx_serial.transfer.file_transfer import FileTransferManager
 
 
 class TestMSXTerminalWithDummy(unittest.TestCase):
-    def setUp(self):
+    @patch("msx_serial.completion.command_completer.IotNodes")
+    def setUp(self, mock_iot_nodes):
+        mock_iot_nodes.return_value.get_node_names.return_value = []
         config = DummyConfig()
         self.conn = DummyConnection(config)
         self.terminal = MSXTerminal(
@@ -20,17 +22,13 @@ class TestMSXTerminalWithDummy(unittest.TestCase):
         self.terminal.user_input = self.user_input
         self.terminal.file_transfer = self.file_transfer
 
-    @patch("msx_serial.completion.loader_iot_nodes.IotNodes.load_nodes_from_yaml")
-    def test_send_and_echo(self, mock_load_nodes):
-        mock_load_nodes.return_value = None
+    def test_send_and_echo(self):
         test_input = 'PRINT "HELLO"'
         self.user_input.send(test_input)
         sent_data = b"".join(self.conn.get_sent_data()).decode("utf-8")
         self.assertIn(test_input, sent_data)
 
-    @patch("msx_serial.completion.loader_iot_nodes.IotNodes.load_nodes_from_yaml")
-    def test_exit_command(self, mock_load_nodes):
-        mock_load_nodes.return_value = None
+    def test_exit_command(self):
         stop_event = self.terminal.stop_event
         handled = self.user_input.handle_special_commands(
             "@exit", self.file_transfer, stop_event
